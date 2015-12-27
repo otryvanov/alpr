d60024c8debe20e8c9b2a4bbf2c7ce956699b38d
Neird Neird <neird.neird@gmail.com>
2015-12-27 21:15:46 +0300

    Implemented sensible 0O conversion in merge_plate

--- python/lib/alpr/tasks/merge_plate.py
+++ python/lib/alpr/tasks/merge_plate.py
@@ -70,6 +70,53 @@ class TaskMergePlate(Task):
             #print i, j ,loc[i], loc[j], o[i][j]#, (overlap(loc[i], loc[j], True)+0.0)#/(li[1][2]*li[1][3])
             pass
 
+    #FIXME works only for type1 russian plate
+    if len(loc)>=8:
+      #by position
+      for i in xrange(len(loc)):
+        letter, box, score = loc[i]
+        X,Y,W,H = box
+        if letter=='0O':
+          if i==0:
+            letter='O'
+          if i>=1 and i<=3:
+            letter='0'
+          if i>=4 and i<=5:
+            letter='O'
+          if i>=6:
+            letter='0'
+
+        loc[i]=(letter, (X,Y,W,H), score)
+    elif len(loc)==0:
+      pass
+    else:
+      #by height statistics
+      hs=np.float32(np.array([p[1][3] for p in loc]))
+      n_cl=min(2, len(loc))
+      criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 0.001)
+      flags = cv2.KMEANS_PP_CENTERS
+      compactness,idx,centers = cv2.kmeans(hs,n_cl,criteria,10,flags)
+      idx=idx.flatten()
+      n_cl=len(list(set(idx)))
+
+      data=[list(hs[idx==i]) for i in xrange(n_cl)]
+      #bigger should have index 1
+      if n_cl>1 and np.mean(data[0])> np.mean(data[1]):
+        idx=1-idx
+      for i in xrange(len(loc)):
+        letter, box, score = loc[i]
+        X,Y,W,H = box
+        if letter=='0O':
+          if i<=len(loc)-3:
+            if idx[i]==1:
+              letter='0'
+            else:
+              letter='O'
+          else:
+            letter='0'
+        loc[i]=(letter, (X,Y,W,H), score)
+
+
     return TaskResultMergePlate(''.join([l[0] for l in loc if l is not None]))
 
 class TaskResultMergePlate(TaskResult):
